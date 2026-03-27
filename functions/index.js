b4919a8517cde4ad2325a7b8541c9b80351a803a
Everson josé <ej061222@gmail.com>
2026-03-27 19:05:02 -0300

    feat: rota de cursos
    - POST /api/cursos - Super Admin cria curso
    - GET /api/cursos - Lista todos os cursos

--- functions/index.js
+++ functions/index.js
@@ -2,18 +2,24 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const admin = require('./config/firebase');
+
 const usuariosRoutes = require('./routes/usuarios');
 const authRoutes = require('./routes/auth');
+const cursosRoutes = require('./routes/cursos');
+
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/usuarios', usuariosRoutes);
-app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
   res.json({ message: 'API funcionando!' });
 });
+
+app.use('/api/auth', authRoutes);
+app.use('/api/usuarios', usuariosRoutes);
+app.use('/api/cursos', cursosRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
